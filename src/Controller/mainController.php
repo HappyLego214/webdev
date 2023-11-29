74d4464aba12a0cfa614458c265603bbe91b152b
BuildTools <unconfigured@null.spigotmc.org>
2023-11-29 23:06:47 +0800

    added pages and updated twig placeholders

--- src/Controller/mainController.php
+++ src/Controller/mainController.php
@@ -11,12 +11,30 @@ class mainController extends AbstractController
     #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
-        return $this->render('page/main/homepage.html.twig');
+        return $this->render('page/main/home.html.twig');
+    }
+
+    #[Route('/sample', name: 'app_sample')]
+    public function samplepage(): Response
+    {
+        return $this->render('page/main/sample.html.twig');
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function aboutpage(): Response
+    {
+        return $this->render('page/main/about.html.twig');
     }
 
     #[Route('/login', name: 'app_login')]
     public function loginpage(): Response
     {
-        return $this->render('page/register/loginpage.html.twig');
+        return $this->render('page/register/login.html.twig');
+    }
+
+    #[Route('/signup', name: 'app_signup')]
+    public function registerpage(): Response
+    {
+        return $this->render('page/register/signup.html.twig');
     }
 }
